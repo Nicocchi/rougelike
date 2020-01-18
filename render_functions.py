3693553bf757e5d0ce9d967833843009b2a7d2ae
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-18 13:21:15 -0500

    using and dropping items implemented

--- render_functions.py
+++ render_functions.py
@@ -2,6 +2,9 @@ import tcod as libtcod
 
 from enum import Enum
 
+from game_states import GameStates
+from menus import inventory_menu
+
 class RenderOrder(Enum):
     CORPSE = 1
     ITEM = 2
@@ -30,7 +33,8 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
     libtcod.console_print_ex(panel, int(x + total_width / 2), y, libtcod.BKGND_NONE, libtcod.CENTER, '{0}: {1}/{2}'.format(name, value, maximum))
 
 # Draw enties and map
-def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors):
+def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, 
+                panel_y, mouse, colors, game_state):
     # Draw all the tiles in the game map
     if fov_recompute:
         for y in range(game_map.height):
@@ -77,6 +81,15 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
     
     libtcod.console_blit(panel, 0, 0, screen_width, panel_height, 0, 0, panel_y)
+
+    # Draw Inventory
+    if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        if game_state == GameStates.SHOW_INVENTORY:
+            inventory_title = 'Press the key next to an item to use it, or Esc to cancel.\n'
+        else:
+            inventory_title = 'Press the key next to an item to drop it, or Esc to cancel.\n'
+            
+        inventory_menu(con, inventory_title, player.inventory, 50, screen_width, screen_height)
 
 # Clears all the entites after drawing to the screen
 def clear_all(con, entities):
