6a2ec6b64ba829ff63e3fe32f95bb77f2cf60396
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-18 00:09:07 -0500

    randomly generated map and field of view placement

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import tcod as libtcod
 
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
@@ -9,12 +10,26 @@ def main():
     # Define screen size
     screen_width = 80
     screen_height = 50
+
+    # Size of the map
     map_width = 80
     map_height = 45
 
+    # Some variables for the room sin the map
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    # Field of view
+    fov_algorithm = 0 # Default algorithm libtcod uses
+    fov_light_walls = True # Whether or not to light up the walls
+    fov_radius = 10 # How far player can see
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     # Define player position -> center screen
@@ -33,6 +48,11 @@ def main():
 
     # Define the game map
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
 
     # Hold the mouse and keyboard input
     key = libtcod.Key()
@@ -43,8 +63,14 @@ def main():
         # Captures new "events", user input. Updates the key and mouse vars with what the user inputs
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
+        # Recompute field of view
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
         # Print the player and set the background to none -> (console, x, y, what to draw, background)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
 
         # Present everything to the screen
         libtcod.console_flush()
@@ -63,8 +89,11 @@ def main():
         # Move the player
         if move:
             dx, dy = move
+
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+
+                fov_recompute = True
         
         # Exit the game
         if exit:
