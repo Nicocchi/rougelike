e10737fade5918c1dee45f09c471edcf0f1b2514
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-27 17:48:49 -0500

    added os checking for linux to save game

--- loader_functions/data_loaders.py
+++ loader_functions/data_loaders.py
@@ -1,21 +1,29 @@
 import os
 import shelve
-
+from sys import platform
 
 def save_game(player, entities, game_map, message_log, game_state):
-    with shelve.open('savegame.dat', 'n') as data_file:
-        data_file['player_index'] = entities.index(player)
-        data_file['entities'] = entities
-        data_file['game_map'] = game_map
-        data_file['message_log'] = message_log
-        data_file['game_state'] = game_state
+    if platform == "linux" or platform == "linux2":
+        with shelve.open('savegame.dat') as data_file:
+            data_file['player_index'] = entities.index(player)
+            data_file['entities'] = entities
+            data_file['game_map'] = game_map
+            data_file['message_log'] = message_log
+            data_file['game_state'] = game_state
+    else:
+        with shelve.open('savegame') as data_file:
+            data_file['player_index'] = entities.index(player)
+            data_file['entities'] = entities
+            data_file['game_map'] = game_map
+            data_file['message_log'] = message_log
+            data_file['game_state'] = game_state
 
 
 def load_game():
     if not os.path.isfile('savegame.dat'):
         raise FileNotFoundError
 
-    with shelve.open('savegame.dat', 'r') as data_file:
+    with shelve.open('savegame.dat') as data_file:
         player_index = data_file['player_index']
         entities = data_file['entities']
         game_map = data_file['game_map']
