c8cc2ee4f3c1a12091a50a961de4b47c83e4e3b5
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-27 17:51:00 -0500

    added os checking for linux to load game

--- loader_functions/data_loaders.py
+++ loader_functions/data_loaders.py
@@ -23,12 +23,20 @@ def load_game():
     if not os.path.isfile('savegame.dat'):
         raise FileNotFoundError
 
-    with shelve.open('savegame.dat') as data_file:
-        player_index = data_file['player_index']
-        entities = data_file['entities']
-        game_map = data_file['game_map']
-        message_log = data_file['message_log']
-        game_state = data_file['game_state']
+    if platform == "linux" or platform == "linux2":
+        with shelve.open('savegame.dat') as data_file:
+            player_index = data_file['player_index']
+            entities = data_file['entities']
+            game_map = data_file['game_map']
+            message_log = data_file['message_log']
+            game_state = data_file['game_state']
+    else:
+        with shelve.open('savegame') as data_file:
+            player_index = data_file['player_index']
+            entities = data_file['entities']
+            game_map = data_file['game_map']
+            message_log = data_file['message_log']
+            game_state = data_file['game_state']
 
     player = entities[player_index]
 
